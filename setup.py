ba4b0bee8d2a396f9020fdb1e2558b3d3c8a18f4
jmcarpenter2 <jmcarpenter2@dons.usfca.edu>
2018-04-18 16:57:56 -0700

    Refactor as package for pip install

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 from distutils.core import setup
 setup(
   name = 'deep_sort_mcdat',
+  packages = ['deep_sort_mcdat'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Customized deep_sort to be aligned with the paper',
   author = 'Jason Carpenter',
   author_email = 'jmcarpenter2@dons.usfca.edu',
   url = 'https://github.com/jmcarpenter2/deep_sort_mcdat', # use the URL to the github repo
+  download_url = 'https://github.com/jmcarpenter2/deep_sort_mcdat/archive/0.1.tar.gz',
+  install_requires=[
+        'numpy',
+        'scipy',
+        'sklearn',
+        'cv2'
+  ],
   classifiers = [],
 )

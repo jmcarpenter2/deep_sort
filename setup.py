3473691bb84b256abad0c927c1476700f5d969e6
jmcarpenter2 <jmcarpenter2@dons.usfca.edu>
2018-04-18 16:41:37 -0700

    include setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+  name = 'deep_sort_mcdat',
+  packages = ['deep_sort_mcdat'], # this must be the same as the name above
+  author = 'Jason Carpenter',
+  author_email = 'jmcarpenter2@dons.usfca.edu',
+  url = 'https://github.com/jmcarpenter2/deep_sort_mcdat', # use the URL to the github repo
+  classifiers = [],
+)

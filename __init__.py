2214d2a74dacb35ddb786fb8868a6b9c6a55013d
jmcarpenter2 <jmcarpenter2@dons.usfca.edu>
2018-04-18 16:47:18 -0700

    init file

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from deep_sort_mcdat.deep_sort_app import run
+from deep_sort_mcdat.tools.generate_detections import create_box_encoder, generate_detections
+
+
+__all__ = ['run', 'create_box_encoder', 'generate_detections']

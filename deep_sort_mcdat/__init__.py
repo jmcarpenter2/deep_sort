0f63640d902f9988b1844ddf247ca4ec5e0f4057
jmcarpenter2 <jmcarpenter2@dons.usfca.edu>
2018-04-18 17:08:32 -0700

    relative imports:

--- deep_sort_mcdat/__init__.py
+++ deep_sort_mcdat/__init__.py
@@ -1,6 +1,6 @@
-from deep_sort_mcdat.application_util import preprocessing
-from deep_sort_mcdat.deep_sort_app import run
-from deep_sort_mcdat.tools.generate_detections import create_box_encoder, generate_detections
+from .application_util import preprocessing
+from .deep_sort_app import run
+from .tools.generate_detections import create_box_encoder, generate_detections
 
 
 __all__ = ['run', 'create_box_encoder', 'generate_detections', 'preprocessing']

9852cd16f39b91b2fa960e80e5c9a8a9cb02c803
jmcarpenter2 <jmcarpenter2@dons.usfca.edu>
2018-04-18 17:01:20 -0700

    Include application util in init

--- deep_sort_mcdat/__init__.py
+++ deep_sort_mcdat/__init__.py
@@ -1,5 +1,6 @@
 from deep_sort_mcdat.deep_sort_app import run
 from deep_sort_mcdat.tools.generate_detections import create_box_encoder, generate_detections
+from deep_sort_mcdat.application_util import preprocessing
 
 
-__all__ = ['run', 'create_box_encoder', 'generate_detections']
+__all__ = ['run', 'create_box_encoder', 'generate_detections', 'preprocessing']

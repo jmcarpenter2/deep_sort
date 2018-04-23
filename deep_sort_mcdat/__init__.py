dd323a8d6136a067e9769a0b5cc07a1caeb869e4
jmcarpenter2 <jmcarpenter2@dons.usfca.edu>
2018-04-23 10:49:13 -0700

    testing application_util import issue

--- deep_sort_mcdat/__init__.py
+++ deep_sort_mcdat/__init__.py
@@ -1,6 +1,6 @@
-from .application_util import preprocessing
+#from .application_util import preprocessing
 from .deep_sort_app import run
 from .tools.generate_detections import create_box_encoder, generate_detections
 
 
-__all__ = ['run', 'create_box_encoder', 'generate_detections', 'preprocessing']
+__all__ = ['run', 'create_box_encoder', 'generate_detections']#, 'preprocessing']

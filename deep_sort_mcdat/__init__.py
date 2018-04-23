0a4bc3aefb854d09246d65d59b69bcf482d79e08
jmcarpenter2 <jmcarpenter2@dons.usfca.edu>
2018-04-23 11:00:24 -0700

    testing relative  imports

--- deep_sort_mcdat/__init__.py
+++ deep_sort_mcdat/__init__.py
@@ -1,4 +1,4 @@
-from application_util import preprocessing
+from ..application_util import preprocessing
 from .deep_sort_app import run
 from .tools.generate_detections import create_box_encoder, generate_detections
 
